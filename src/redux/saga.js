074d5361adc103fd45310a8db93ff4887be7db95
HH <gygud98@naver.com>
2021-02-06 19:16:20 +0900

    210206 redux-saga fork 수정

--- src/redux/saga.js
+++ src/redux/saga.js
@@ -1,11 +1,11 @@
-import {all} from 'redux-saga/effects'
+import {all, call} from 'redux-saga/effects'
 import appSaga from './app/saga';
 import authSaga from './auth/saga';
 
 function* sagas() {
     yield all([
-        appSaga(),
-        authSaga(),
+        call(appSaga),
+        call(authSaga),
     ])
 }
 
